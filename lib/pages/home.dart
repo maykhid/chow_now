8e61f0d52f2ac243ae5e3a5987af211829e4bbac
Henry Ifebunandu <henryifebunandu@gmail.com>
2022-05-12 08:50:41 +0100

    feat: transform square with animation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:chow_now/controller/bottom_sheet_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -207,47 +210,105 @@ class SheetChild extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.fastOutSlowIn,
-      height: _heightOfModalSheet,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 80,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Container(
-                      // color: Colors.black,
-                      width: 50,
-                      height: 10,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(10),
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.fastOutSlowIn,
+          height: _heightOfModalSheet,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Container(
+                          // color: Colors.black,
+                          width: 50,
+                          height: 10,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  const Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'All Foods',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w600,
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'All Foods',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                GridViewWidget(widgetList: widgetList),
+              ],
             ),
-            GridViewWidget(widgetList: widgetList),
-          ],
+          ),
         ),
-      ),
+        AnimatedOpacity(
+          opacity: (_heightOfModalSheet > 500) ? 1.0 : 0.0,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.fastOutSlowIn,
+          child: AnimatedPadding(
+            curve: Curves.fastOutSlowIn,
+              padding: EdgeInsets.only(bottom: (_heightOfModalSheet > 500) ? 80.0 : 100.0,),
+              duration: const Duration(milliseconds: 500),
+              child: const TransformSquare()),
+        ),
+      ],
+    );
+  }
+}
+
+class TransformSquare extends StatelessWidget {
+  const TransformSquare({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        // main square
+        Transform.rotate(
+          angle: pi / 4,
+          child: Container(
+            height: 110,
+            width: 110,
+            padding: const EdgeInsets.only(top: 500, bottom: 500),
+            decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(25))),
+          ),
+        ),
+        // circle
+        Container(
+          height: 60,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            color: Colors.black,
+            shape: BoxShape.circle,
+          ),
+        ),
+
+        // searck icon
+        const Icon(
+          FontAwesomeIcons.search,
+          color: Colors.white,
+          size: 24,
+        )
+      ],
     );
   }
 }
